0e476d9d50d66aee925b4e23dd8a4004a0cff66c
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-03 10:17:40 +0100

    implement values of username, password, token url from application.yml

--- src/main/java/com/pgs/client/service/ClientService.java
+++ src/main/java/com/pgs/client/service/ClientService.java
@@ -3,6 +3,7 @@ package com.pgs.client.service;
 import com.pgs.client.dto.TokenDto;
 import com.pgs.client.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,11 +14,15 @@ public class ClientService {
 
     public final RestTemplate restTemplate;
 
-    private static final String TOKEN_URL = "http://localhost:8080/oauth/token";
-
+    @Value("${app.user.name}")
+    private String username;
+    @Value("${app.user.password}")
+    private String password;
+    @Value("${app.token.url}")
+    private String token;
     public TokenDto getToken() {
-        var request = new HttpEntity<>(new UserDto("admin", "admin", "password"));
-        return restTemplate.postForObject(TOKEN_URL, request, TokenDto.class);
+        var request = new HttpEntity<>(new UserDto(username, password, "password"));
+        return restTemplate.postForObject(token, request, TokenDto.class);
     }
 
 }
