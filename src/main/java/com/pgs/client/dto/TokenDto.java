d58aa112a160e53a104ad28043bb11040c45521e
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-03 09:24:20 +0100

    added @Data and @Builder

--- src/main/java/com/pgs/client/dto/TokenDto.java
+++ src/main/java/com/pgs/client/dto/TokenDto.java
@@ -1,5 +1,10 @@
 package com.pgs.client.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class TokenDto {
     private String access_token;
     private String token_type;
