73b109cd2a80f48575dbaaf05ca1971f56e4df67
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-03 13:29:07 +0100

    changed name to AuthenticationClient and changed the implementation of getToken method

---
+++ src/main/java/com/pgs/client/service/AuthenticationClient.java
@@ -0,0 +1,44 @@
+package com.pgs.client.service;
+
+import com.pgs.client.dto.Token;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationClient {
+
+    public final RestTemplate authRestTemplate;
+
+    @Value("${app.user.name}")
+    private String username;
+    @Value("${app.user.password}")
+    private String password;
+    @Value("${app.user.grantType}")
+    private String grantType;
+    @Value("${app.token.url}")
+    private String tokenUrl;
+
+    public Token getToken() {
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("grant_type", grantType);
+        requestBody.add("username", username);
+        requestBody.add("password", password);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        var request = new HttpEntity<>(requestBody, headers);
+        return authRestTemplate.postForObject(
+                tokenUrl,
+                request,
+                Token.class);
+    }
+}
