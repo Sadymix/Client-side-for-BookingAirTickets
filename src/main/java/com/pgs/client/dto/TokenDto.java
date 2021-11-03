7ea039e604e26595eabacfd8a239a373240c7dc2
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-03 08:56:50 +0100

    added TokenDto class

---
+++ src/main/java/com/pgs/client/dto/TokenDto.java
@@ -0,0 +1,9 @@
+package com.pgs.client.dto;
+
+public class TokenDto {
+    private String access_token;
+    private String token_type;
+    private String refresh_type;
+    private Integer expires_in;
+    private String scope;
+}
