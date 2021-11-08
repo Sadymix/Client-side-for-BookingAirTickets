0c035ec6615331dab6c4b6d45ac115e3ba2631d1
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-08 11:36:14 +0100

    implemented @ActiveProfiles

--- src/test/java/com/pgs/client/service/UserClientTest.java
+++ src/test/java/com/pgs/client/service/UserClientTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,6 +27,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("smoke-test")
 class UserClientTest {
 
     @Captor
