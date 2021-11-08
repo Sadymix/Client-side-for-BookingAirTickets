fad13966075309d3b82618edd330f19fda862958
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-08 12:11:15 +0100

    modified url in application.yml and UserClientTest.java

--- src/test/java/com/pgs/client/service/UserClientTest.java
+++ src/test/java/com/pgs/client/service/UserClientTest.java
@@ -27,7 +27,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@ActiveProfiles("default")
+@ActiveProfiles()
 class UserClientTest {
 
     @Captor
