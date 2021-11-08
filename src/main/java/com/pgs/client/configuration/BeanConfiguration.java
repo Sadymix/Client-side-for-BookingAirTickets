d0fed83bee4a003e491aa2d20f515603345d2a0e
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-08 12:40:57 +0100

    modified configuration of RestTemplate bean for logging purpose

--- src/main/java/com/pgs/client/configuration/BeanConfiguration.java
+++ src/main/java/com/pgs/client/configuration/BeanConfiguration.java
@@ -1,11 +1,17 @@
 package com.pgs.client.configuration;
 
+import com.pgs.client.interceptor.LoggingInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,6 +32,13 @@ public class BeanConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        var restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+        if(CollectionUtils.isEmpty(interceptors)) {
+            interceptors = new ArrayList<>();
+        }
+        interceptors.add(new LoggingInterceptor());
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 }
