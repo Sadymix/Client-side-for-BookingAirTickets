ad40b5c15c2efdb9fddd2bf6cf0ef0d22e448074
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-15 16:15:33 +0100

    modified isAccessTokenNeeded method to check for seconds instead of hours

--- src/main/java/com/pgs/client/supplier/AccessTokenSupplier.java
+++ src/main/java/com/pgs/client/supplier/AccessTokenSupplier.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -30,7 +29,7 @@ public class AccessTokenSupplier {
     }
 
     private boolean isAccessTokenNeeded(){
-        return stopWatch.getTime(TimeUnit.HOURS) >= ttl.get(ChronoUnit.HOURS)
+        return stopWatch.getTime(TimeUnit.SECONDS) >= ttl.toSeconds()
                 || ACCESS_TOKEN == null
                 || !stopWatch.isStarted();
     }
