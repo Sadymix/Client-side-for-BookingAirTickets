2792c38bd7200280d7a25e118627b2313181d416
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-16 13:57:33 +0100

    implemented changes for test case testIntercept and removed redundant code

--- src/test/java/com/pgs/client/interceptor/AuthenticationInterceptorTest.java
+++ src/test/java/com/pgs/client/interceptor/AuthenticationInterceptorTest.java
@@ -8,11 +8,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.mock.http.client.MockClientHttpRequest;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
@@ -28,36 +25,19 @@ class AuthenticationInterceptorTest {
     @InjectMocks
     private AuthenticationInterceptor authenticationInterceptor;
 
-    private MockClientHttpRequest mockClientHttpRequest;
-    private static byte[] BODY = null;
-
-    private static final String TOKEN = "ASDF";
-
     @SneakyThrows
     @BeforeEach
     void setUp() {
         when(accessTokenSupplier.supplyToken())
-                .thenReturn(TOKEN);
-        when(mockClientHttpRequest.getHeaders())
-                .thenReturn(new HttpHeaders(getValueMap()));
+                .thenReturn("ASDF");
     }
 
     @SneakyThrows
     @Test
     void testIntercept() {
-       var response=  authenticationInterceptor.intercept(
-                mockClientHttpRequest,
-                BODY,
-                clientHttpRequestExecution);
-        verify(clientHttpRequestExecution).execute(mockClientHttpRequest, BODY);
+        var request = new MockClientHttpRequest();
+        authenticationInterceptor.intercept(request, null, clientHttpRequestExecution);
         verify(accessTokenSupplier).supplyToken();
-        assertTrue(response.getHeaders().containsKey("Authorization"));
-    }
-
-    private MultiValueMap<String, String> getValueMap() {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.add("headers", "headers");
-        headers.add("Authorization", "ASDASD");
-        return headers;
+        assertTrue(request.getHeaders().containsKey("Authorization"));
     }
 }
