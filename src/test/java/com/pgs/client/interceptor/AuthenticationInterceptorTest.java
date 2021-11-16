eea447e7ad3d66c14ad7289cd94ca3ae52d92966
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-16 09:33:58 +0100

    removed redundant test case TestIntercept10Threads, modified setUp method, removed redundant mock

--- src/test/java/com/pgs/client/interceptor/AuthenticationInterceptorTest.java
+++ src/test/java/com/pgs/client/interceptor/AuthenticationInterceptorTest.java
@@ -12,17 +12,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -37,8 +31,6 @@ class AuthenticationInterceptorTest {
     private ClientHttpRequestExecution clientHttpRequestExecution;
     @InjectMocks
     private AuthenticationInterceptor authenticationInterceptor;
-    @Mock
-    private ClientHttpResponse clientHttpResponse;
 
     private static byte[] BODY = new byte[20];
 
@@ -52,35 +44,19 @@ class AuthenticationInterceptorTest {
         new Random().nextBytes(BODY);
         when(accessTokenSupplier.supplyToken())
                 .thenReturn(TOKEN.getAccessToken());
-        when(clientHttpRequestExecution.execute(httpRequest, BODY))
-                .thenReturn(clientHttpResponse);
+        when(httpRequest.getHeaders())
+                .thenReturn(new HttpHeaders(getValueMap()));
     }
 
     @SneakyThrows
     @Test
     void testIntercept() {
-        assertFalse(clientHttpResponse.getHeaders().containsKey("Authorization"));
         authenticationInterceptor.intercept(
                 httpRequest,
                 BODY,
                 clientHttpRequestExecution);
-        when(httpRequest.getHeaders())
-                .thenReturn(new HttpHeaders(getValueMap()));
         verify(clientHttpRequestExecution).execute(httpRequest, BODY);
         verify(accessTokenSupplier).supplyToken();
-    }
-
-    @SneakyThrows
-    @Test
-    void TestIntercept10Threads() {
-        when(clientHttpResponse.getHeaders()).thenReturn(new HttpHeaders(getValueMap()));
-        ExecutorService service = Executors.newFixedThreadPool(10);
-        Future<ClientHttpResponse> future = null;
-        for (int i = 0; i < 10; i++) {
-            future = service.submit(() ->
-                    authenticationInterceptor.intercept(httpRequest, BODY, clientHttpRequestExecution));
-        }
-        assertTrue(future.get().getHeaders().containsKey("Authorization"));
     }
 
     private MultiValueMap<String, String> getValueMap() {
