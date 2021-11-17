63f0c1f327579bb757da554183c45eab0de1568b
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-17 12:53:53 +0100

    implemented smoke-test for all clients

--- src/main/java/com/pgs/client/component/Client.java
+++ src/main/java/com/pgs/client/component/Client.java
@@ -1,13 +1,14 @@
 package com.pgs.client.component;
 
-import com.pgs.client.dto.UserDto;
-import com.pgs.client.service.UserClient;
+import com.pgs.client.dto.*;
+import com.pgs.client.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -17,8 +18,12 @@ import java.util.List;
 public class Client {
 
     private final UserClient userClient;
+    private final ReservationClient reservationClient;
+    private final PassengerClient passengerClient;
+    private final AirportClient airportClient;
+    private final FlightClient flightClient;
+    private final WeatherClient weatherClient;
 
-    public static String TOKEN;
     private static UserDto userDto = UserDto.builder()
             .username("user0")
             .password("pass0")
@@ -28,18 +33,89 @@ public class Client {
             .enabled(true)
             .roles(List.of("USER"))
             .build();
+    private static PassengerDto passengerDto = PassengerDto.builder()
+            .firstName("Jim")
+            .lastName("One")
+            .email("JimOne@nose.org")
+            .country("USA")
+            .telephone("301242142")
+            .build();
+    private static PassengerDto passengerDto1 = PassengerDto.builder()
+            .firstName("One")
+            .lastName("One")
+            .email("oneone@nose.org")
+            .country("USA")
+            .telephone("123141242")
+            .build();
+    private static ReservationDto reservationDto = ReservationDto.builder()
+            .flightId(1L)
+            .status(ReservationDto.ReservationStatus.IN_PROGRESS)
+            .passengers(List.of(passengerDto))
+            .userId(2L)
+            .build();
+    private static AirportDto airportDto = AirportDto.builder()
+            .code("ASD")
+            .name("Airport")
+            .country("USA")
+            .build();
+    private static FlightDto flightDto = FlightDto.builder()
+            .type(FlightDto.TypeOfFlight.ECONOMY)
+            .departureDate(LocalDateTime.now())
+            .arrivalDate(LocalDateTime.now())
+            .departureAirportIataCode("TLG")
+            .arrivalAirportIataCode("HAE")
+            .build();
 
     @PostConstruct
     public void postConstruct() {
-        var singleUser = userClient.getSingleUser(1L);
-        log.info("Single User: {}", singleUser);
-        var addUser = userClient.addUser(userDto);
-        log.info("Added User: {}", addUser);
-        var deactivateUser = userClient.deactivateUser(1L);
-        log.info("Deactivate User: {}", deactivateUser);
-        var activateUser = userClient.activateUser(1L);
-        log.info("Activated User: {}", activateUser);
-        var setRolesUser = userClient.setUserRoles(1L, List.of("USER", "STAFF"));
-        log.info("Set Roles User: {}", setRolesUser);
+        userClientPostConstruct();
+        reservationClientPostConstruct();
+        passengerClientPostConstruct();
+        airportClientPostConstruct();
+        flightClientPostConstruct();
+        weatherClient.getWeatherByCity("Warsaw");
     }
+
+    private void userClientPostConstruct() {
+        userClient.getSingleUser(1L);
+        userClient.addUser(userDto);
+        userClient.deactivateUser(1L);
+        userClient.activateUser(1L);
+        userClient.setUserRoles(1L, List.of("USER", "STAFF"));
+    }
+
+    private void reservationClientPostConstruct() {
+        reservationClient.getReservationWithPassengersAndFlight(2L);
+        reservationClient.getReservationsByFlight(1L);
+        reservationClient.getReservationsForCurrentUser();
+        reservationClient.getReservationsByUser(1L);
+        reservationClient.addReservation(reservationDto);
+        reservationClient.cancelReservation(2L);
+        reservationClient.realizedReservation(2L);
+        reservationClient.deleteReservation(1L);
+    }
+
+    private void passengerClientPostConstruct() {
+        passengerClient.getPassengers();
+        passengerClient.getSinglePassenger(2L);
+        passengerClient.addPassenger(passengerDto);
+        passengerClient.editPassenger(passengerDto1, 4L);
+    }
+
+    private void airportClientPostConstruct() {
+        airportClient.getAirports();
+        airportClient.getAirportById(1L);
+        airportClient.getAirportByCode("YYC");
+        airportClient.addAirport(airportDto);
+        airportClient.editAirport(airportDto, 123L);
+    }
+
+    private void flightClientPostConstruct() {
+        flightClient.getFlights();
+        flightClient.getFlight(4L);
+        flightClient.addFlight(flightDto);
+        flightClient.editFlight(flightDto, 1L);
+        flightClient.deleteFlight(3L);
+    }
+
 }
